23d7f1e9f8d0744be45bc0f3d385aa421e15e715
jfelipeschwaab <joaofsp.games@gmail.com>
2025-11-11 14:21:05 -0300

    Initial Commit

---
+++ NubankFake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NubankFake
+//
+//  Created by João Felipe Schwaab on 11/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
